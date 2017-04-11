09a328bfbdec7078576469abb30003e56b8b94f3
WolfWang <1678173987@qq.com>
2017-04-11 17:34:08 +0800

    添加application

---
+++ app/src/main/java/com/gzh/wolfwang/rxjavaretrofit2framework/base/MyApplication.java
@@ -0,0 +1,22 @@
+package com.gzh.wolfwang.rxjavaretrofit2framework.base;
+
+import android.app.Application;
+
+/**
+ * author：WolfWang
+ * date：2017/4/11 17:00
+ * e-mail：1678173987@qq.com
+ * description：全局application
+ */
+
+public class MyApplication extends Application{
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+
+    }
+}
